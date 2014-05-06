2044f5225285b7834e5f28f52fcbb94065e9857c
Andrew <ndrewniklas@gmail.com>
2014-05-05 18:33:56 -0700

    Signed-off-by: Andrew <ndrewniklas@gmail.com>

---
+++ Group Project/src/edu/csupomona/cs/cs141/thehouse/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.csupomona.cs.cs141.thehouse;
+
+/**
+ * @author Andrew
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
