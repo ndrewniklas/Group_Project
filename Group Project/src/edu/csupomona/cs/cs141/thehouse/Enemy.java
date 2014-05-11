cbf0be061c0213958c0e620ec81200ea392a1639
Dylan Nguyen <dylanminhnguyen@gmail.com>
2014-05-11 14:37:08 -0700

    Final checks?

--- Group Project/src/edu/csupomona/cs/cs141/thehouse/Enemy.java
+++ Group Project/src/edu/csupomona/cs/cs141/thehouse/Enemy.java
@@ -1,5 +1,58 @@
+/**
+ * CS 141: Introduction to Programming and Problem Solving
+ * Professor: Edwin Rodr&iacute;guez
+ *
+ * Programming Group Project
+ *
+ *   Team BAAKD
+ *   Ben Nickerson
+ *   Andrew Nipp
+ *   Dylan Nguyen
+ *   Kurt Newcomb
+ *   Andrew Niklas 
+ */
+
+
 package edu.csupomona.cs.cs141.thehouse;
 
 public class Enemy {
 
+	boolean ninjaLive;
+	boolean ninjaDead;
+	
+	int initialX;
+	int initialY;
+	int xPosition;
+	int yPosition;
+	int ninjaLife;
+	int roomNumber;
+	
+	/**
+	 * {@link #moveNinja()} This method will move the ninja
+	 */
+	public void moveNinja() {
+		
+	}
+	
+	/**
+	 * {@link #checkForPlayer()} This method will let the enemy check if the player is
+	 * in the adjacent space
+	 */
+	public void checkForPlayer() {
+		
+	}
+	
+	/**
+	 * {@link #killPlayer()} This method will kill the player
+	 */
+	public void killPlayer() {
+		
+	}
+	
+	/**
+	 * {@link #ninjaDies()} This method will kill the ninja if the player fires a bullet
+	 */
+	public void ninjaDies() {
+		
+	}
 }
