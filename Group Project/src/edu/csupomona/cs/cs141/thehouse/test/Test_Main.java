5aee7876b9bb9e1ca1668206ca1ad5756cb87908
amnipp <amnipp@csupomona.edu>
2014-05-12 11:26:25 -0700

    Package Created: This package is for testing purposes, if you have to test out code then create a method within the Test_Main class Todo: Finish Grid and test it

---
+++ Group Project/src/edu/csupomona/cs/cs141/thehouse/test/Test_Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package edu.csupomona.cs.cs141.thehouse.test;
+
+/**
+ * @author Andrew
+ *
+ */
+public class Test_Main {
+
+	/**
+	 * 
+	 */
+	public Test_Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
