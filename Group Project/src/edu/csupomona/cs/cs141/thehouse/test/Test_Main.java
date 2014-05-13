040659a7bd29ce61c5b119b12fde806bd10bb57d
Andrew <ndrewniklas@gmail.com>
2014-05-12 22:06:47 -0700

    Signed-off-by: Andrew <ndrewniklas@gmail.com>

--- Group Project/src/edu/csupomona/cs/cs141/thehouse/test/Test_Main.java
+++ Group Project/src/edu/csupomona/cs/cs141/thehouse/test/Test_Main.java
@@ -2,6 +2,8 @@
  * 
  */
 package edu.csupomona.cs.cs141.thehouse.test;
+import java.util.Scanner;
+
 import edu.csupomona.cs.cs141.thehouse.*;
 /**
  * @author Andrew
@@ -16,24 +18,53 @@ public class Test_Main {
 		// TODO Auto-generated method stub
 		printGrid_Test1();
 		printGrid_Test2();
+//		movementTest_Basic();
+		playerMove();
 	}
 	
 	public static void printGrid_Test1(){
 		System.out.println("Grid: Print Test 1");
 		System.out.println("R is the room");
 		Grid grid = new Grid();
-		grid.populateGrid();
-		grid.printGrid();
 		System.out.println("-----------------------------------------------------");
 	}
+	
 	public static void printGrid_Test2(){
 		System.out.println("Grid: Print Test 2");
 		System.out.println("B is the briefcase, R is the room, P is the player");
 		Grid grid = new Grid();
-		grid.populateGrid();
 		grid.setBriefcase();
-		grid.printGrid();
 		
 		System.out.println("-----------------------------------------------------");
+	}
+	
+	public static void movementTest_Basic() {
+		System.out.println("Player movement test 1");
+		Player ply = new Player();
+		
+		while (true) {
+			for(int i:ply.getPosition()){
+				System.out.println(i);
+			}
+			Scanner sc = new Scanner(System.in);
+			String input = sc.next();
+			ply.movePlayer(input);
+			
+		}
+	}
+	
+	public static void playerMove() {
+		System.out.println("Player movement test 2");
+		Player ply = new Player();
+		Grid grid = new Grid();
+		
+		String input="h";
+		while(input!= "Q"){
+			Scanner sc = new Scanner(System.in);
+			input=sc.next();
+			ply.movePlayer(input);
+			grid.printGrid();
+			
+		}
 	}
 }
