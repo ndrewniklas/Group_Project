b669d401e603d81206c502561f89eb59fe0870cf
Ben Nickerson <bendnickerson@gmail.com>
2014-05-23 23:36:32 -0700

    Hopefully This works... and doesnt delete any essential information.

--- Group Project/src/edu/csupomona/cs/cs141/thehouse/GameEngine.java
+++ Group Project/src/edu/csupomona/cs/cs141/thehouse/GameEngine.java
@@ -17,6 +17,8 @@
 
 package edu.csupomona.cs.cs141.thehouse;
 
+import java.util.Scanner;
+
 /**
  * 
  *	GameEngine is where almost everything from the other classes contribute to the game.
@@ -29,6 +31,11 @@ package edu.csupomona.cs.cs141.thehouse;
 public class GameEngine {
 	
 	/**
+	 * Instantiation for {@link Scanner}
+	 */
+	private Scanner in = new Scanner(System.in);
+	
+	/**
 	 * Instantiation for {@link Grid}
 	 */
 	private Grid grid;
@@ -39,6 +46,11 @@ public class GameEngine {
 	private Player plr;
 	
 	/**
+	 * Instantiation for {@link UserInterface}
+	 */
+	private UserInterface ui;
+	
+	/**
 	 * Checks to see if the game is over
 	 */
 	private boolean gameOver;
@@ -47,6 +59,42 @@ public class GameEngine {
 	 * Checks to see if the briefcase is found
 	 */
 	private boolean foundBriefcase;
+<<<<<<< HEAD
+	
+	/** 
+	 * Checks to see if the user quit 
+	 */
+	private boolean userQuit;
+	
+	/**
+	 * This field holds the choice of the user. There are xx potential values for the user to select:
+	 * left,
+	 * right,
+	 * up,
+	 * down,
+	 * shoot,
+	 * save,
+	 * exit
+	 */
+	private String userChoice;
+	
+	private String moveChoice;
+
+	/**
+	 * This field holds the choice of direction for which the user will "look" before each turn.
+	 * There are four directions: 
+	 * left,
+	 * right,
+	 * up,
+	 * down
+	 */
+	private String lookDirection;
+
+	/**
+	 * This field holds a string set by the user. It can be any of the following:
+	 * 
+	 */
+	private String menuSelection;
 
 	private UserInterface ui;
 
@@ -57,6 +105,12 @@ public class GameEngine {
 	private String cmd;
 	
 	/**
+	 * This field will repeat a do while loop if {@code true} in either {@link #mainMenuSelect()}
+	 * or {@link #returnToMain()}.
+	 */
+	private boolean repeat;
+
+	/**
      * The default constructor for GameEngine.
      * 
      * Currently it is only filling up the instances of the classes and forcing {@link #gameOver}
@@ -66,17 +120,92 @@ public class GameEngine {
     	gameOver = false;
     	foundBriefcase=false;
     	plr = new Player();
+    	ui = new UserInterface();
+    	mainMenuSelect();
     	grid = new Grid();
     	ui = new UserInterface();
     	
     }
     
+    //For use with navigating the main menu
+    /**
+     * This method is called after {@link UserInterface#mainMenu()} prints the menu. It uses a 
+     * switch statement to check each potential command, and a do-while loop to ensure that 
+     * proper input is received. When {@code default} is used, the code repeats, otherwise a 
+     * specific command is called:
+     * Specifically, "new" starts a new game
+     * 			   , "continue" continues the game from its last state
+     * 			   , "rules" calls {@link UserInterface#rules()}
+     * 			   , "options" calls {@link UserInterface#options()}
+     * 			   , "exit" exits the program 
+     */
+    private void mainMenuSelect() {
+		do {
+    		menuSelection = in.next();
+    		menuSelection = menuSelection.toLowerCase();
+    		repeat = false;
+    		switch(menuSelection) {
+			case "new": 
+				gameLoop();
+				break;
+			case "continue":
+				break;
+			case "rules":
+				ui.rules();
+				returnToMain();
+				break;
+			case "options":
+				ui.options();
+				returnToMain();
+				break;
+			case "exit":
+				System.exit(0);
+				break;
+			default:
+				ui.invalid();
+				repeat = true;
+				break;
+			}
+		} while(repeat == true);
+	}
+
+	/**
+	 * This method contains a switch statement and do while loop that essentially serves to return the 
+	 * user to the main menu.
+	 */
+	private void returnToMain() {
+		do{
+			menuSelection = in.next();
+			menuSelection = menuSelection.toLowerCase();
+			switch (menuSelection) {
+			case "return":
+				ui.mainMenu();
+				mainMenuSelect();
+				break;
+			default:
+				ui.invalid();
+				repeat = true;
+				break;
+			}
+		} while(repeat == true);
+	}
+
+	// Main game methods
     /**
      * The main loop of the game, this is where all the checks and actions go.
      * The loop is finished once {@link #gameOver} is set to be true either from
      * finding the briefcase, the player dying, or the user quitting.
      */
     public void gameLoop(){
+    	grid.setBriefcase();
+    	grid.setEnemy();
+    	grid.rePopulateGrid(plr);
+    	ui.printGrid(grid);
+    	while(true) {
+    		ui.topTurn();
+    		topTurnSelect();
+    		grid.rePopulateGrid(plr);
+    		ui.printGrid(grid);
     	while(!gameOver){
     		grid.printGrid();
     		ui.turnMenu(); //print options available during each turn
@@ -100,9 +229,64 @@ public class GameEngine {
 			default:
 				break;
 			}
-    		plr.movePlayer(input);
-    		
+    		plr.movePlayer(input);    		
     	}
+    }
+    
+    private void topTurnSelect() {
+		do{
+			userChoice = in.next();
+			userChoice = userChoice.toLowerCase();
+			repeat = false;
+			switch(userChoice) {
+			case "look":
+				break;
+			case "move":
+				ui.moveTurn();
+				moveChoice = in.next();
+				plr.movePlayer(moveChoice, ui);
+				break;
+			case "shoot":
+				break;
+			case "options":
+				break;
+			case "exit":
+				System.exit(0);
+				break;
+			default:
+				ui.invalid();
+				repeat = true;
+				break;
+			}
+		} while(repeat == true);
+	}
+
+	/**
+     * The method that will retrieve the position of each room from {@link Grid} and save 
+     * them to the corresponding local 2d String array.
+     */
+    public void setRoomPositions(){
+    	
+    }
+    
+    // Methods to check/send fields to other classes    
+    /**
+     * Used to set {@link #lookDirection} and {@link #userChoice}.
+     * @param
+     * 		lookDir - The direction chosen by the user to look, saved to {@link #lookDirection}
+     * @param
+     * 		choice - The choice of the user saved to a {@link #userChoice}. 		 
+     */		
+    public void setUserCMD(String lookDir, String choice){
+    	
+    }
+
+    /**
+     * Used to send out {@link #userQuit} which is checked by {@link #performUserAction()}.
+     * @return {@link #userQuit} value
+     */
+    public boolean userQuitGame(){
+    	return userQuit;
     }
 
     /**
--- Group Project/src/edu/csupomona/cs/cs141/thehouse/UserInterface.java
+++ Group Project/src/edu/csupomona/cs/cs141/thehouse/UserInterface.java
@@ -26,14 +26,13 @@ public class UserInterface {
 	private String input;
 	
 	Scanner sc = new Scanner(System.in);
-
 	/**
 	 * This constructor will build the {@link UserInterface} for use with {@link GameEngine}.
 	 * Then it will check after each round whether or not the game has ended and print out the appropriate
 	 * results.
 	 */
 	public UserInterface() {
-		
+		mainMenu();
 	}
 	
 	/**
@@ -41,25 +40,85 @@ public class UserInterface {
 	 * 		or "new game".
 	 */
 	public void mainMenu() {
-		
+		System.out.println("Welcome to the House Game!");
+		System.out.println();
+		System.out.println("What would you like to do?");
+		System.out.println("New");
+		System.out.println("Continue");
+		System.out.println("Rules");
+		System.out.println("Options");
+		System.out.println("Exit");
+		System.out.println("Please enter one of the commands:");
+	}
+	
+	/**
+	 * This method is called by {@link #main Menu()}. 
+	 */
+	public void rules() {
+		System.out.println("The rules to the game are very simple:");
+		System.out.println("You will be spawned in the bottom left-hand corner of a building. There will "
+						 + "also be six enemies in the building with you. \nLocated throughout the building "
+						 + "are nine rooms and in one of these rooms is a briefcase. \nFind the briefcase"
+						 + "and win the game.");
+		System.out.println();
+		System.out.println("User Commands: ");
+		System.out.println("You have several options available to you while the game is running.");
+		System.out.println("1. Left - moves you to the left one space");
+		System.out.println("2. Right - moves you to the right one space");
+		System.out.println("3. Up - moves you up one space");
+		System.out.println("4. Down - moves you down one space");
+		System.out.println("5. Look - you can use this at the start of every round, it lets you see two"
+						          + " blocks in any direction.");
+		System.out.println("6. Shoot - you can use this if you have a bullet left. It will kill one enemy"
+								   + " if in the path of the bullet.");
+		System.out.println("7. Save - use this to save the game, and return to it later by selecting"
+				                  + " 'continue' in the main menu.");
+		System.out.println("8. Exit - use this to quit the game, be aware that progress will not be saved.");
+		System.out.println("Type \"return\" to return to the main menu:");
+	}
+	
+	public void options() {
+		System.out.println("You can change the options here:");
+		System.out.println("1. AI - ");
+		System.out.println("2. ");
 	}
 	 
 	/**
 	 * This method prints the {@link Grid} in its current state. This will occur after every round. It will
 	 * pull the {@link Grid} class from {@link GameEngine#getGrid()} and then call {@link Grid#getfullGrid()}
+	 * @param grid 
 	 */
-	public void printGrid() {
-		
+	public void printGrid(Grid grid) {
+		grid.printGrid();
 	}
 
-	/**
-	 * 
-	 */
+	public void topTurn() {
+		System.out.println("What would you like to do?");
+		System.out.println("Look");
+		System.out.println("Move");
+		System.out.println("Shoot");
+		System.out.println("Options");
+		System.out.println("Exit");
+		System.out.println("Please enter one of the commands:");
+
 	public void welcome() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	public void moveTurn() {
+		System.out.println("Which direction will you move?");
+		System.out.println("Up");
+		System.out.println("Down");
+		System.out.println("Left");
+		System.out.println("Right");
+		System.out.println("Please enter one of the commands:");
+	}
+
+	public void invalid() {
+		System.out.println("Invalid Command: Try Again");
+		
+	}
 	/**
 	 * 
 	 */
